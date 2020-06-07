a03793a09939aa688be3792936af81a51396aac5
MapMaths <62785981+MapMaths@users.noreply.github.com>
2020-06-07 21:52:45 +0800

    Update body.js

--- classtable/body.js
+++ classtable/body.js
@@ -1,3 +1,23 @@
 var w = new Date();
 var day = w.getDay() + 1;
-document.write("<table border='3'><tr><th>时段</th><th>时间</th><th");
+function displayDate(var fday){
+  if (day == fday)
+  {
+    document.write("<th class='red'>");
+  }
+  else
+  {
+    document.write("<th class='blue'>");
+  }
+}
+document.write("<table border='3'><tr><th>时段</th><th>时间</th><th>节数</th>");
+disPlayDate(1);
+document.write("星期一</th>");
+disPlayDate(2);
+document.write("星期二</th>");
+disPlayDate(3);
+document.write("星期三</th>");
+disPlayDate(4);
+document.write("星期四</th>");
+disPlayDate(5);
+document.write("星期五</th>");
