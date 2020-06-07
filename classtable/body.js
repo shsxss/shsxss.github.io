584762f43e549e01c4f019879963a696d6411dd9
MapMaths <62785981+MapMaths@users.noreply.github.com>
2020-06-08 06:34:12 +0800

    Update body.js

--- classtable/body.js
+++ classtable/body.js
@@ -1,23 +1,34 @@
 var w = new Date();
-var day = w.getDay() + 1;
-function displayDate(var fday){
-  if (day == fday)
-  {
-    document.write("<th class='red'>");
-  }
-  else
-  {
-    document.write("<th class='blue'>");
-  }
+var day = w.getDay();
+document.write("<table border='3'><tr><th class='blue'>时段</th><th class='blue'>时间</th><th class='blue'>节数</th>");
+if (day == 1){
+  document.write("<th class='red'>星期一</th>");
 }
-document.write("<table border='3'><tr><th>时段</th><th>时间</th><th>节数</th>");
-disPlayDate(1);
-document.write("星期一</th>");
-disPlayDate(2);
-document.write("星期二</th>");
-disPlayDate(3);
-document.write("星期三</th>");
-disPlayDate(4);
-document.write("星期四</th>");
-disPlayDate(5);
-document.write("星期五</th>");
+else{
+  document.write("<th class='blue'>星期一</th>");
+}
+if (day == 2){
+  document.write("<th class='red'>星期二</th>");
+}
+else{
+  document.write("<th class='blue'>星期二</th>");
+}
+if (day == 3){
+  document.write("<th class='red'>星期三</th>");
+}
+else{
+  document.write("<th class='blue'>星期三</th>");
+}
+if (day == 4){
+  document.write("<th class='red'>星期四</th>");
+}
+else{
+  document.write("<th class='blue'>星期四</th>");
+}
+if (day == 5){
+  document.write("<th class='red'>星期五</th>");
+}
+else{
+  document.write("<th class='blue'>星期五</th>");
+}
+document.write("</tr>");
