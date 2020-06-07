1a7b3ca8ec7e8980da2a52ff04e3e05cdbd47fd7
MapMaths <62785981+MapMaths@users.noreply.github.com>
2020-06-07 17:13:34 +0800

    Update and rename main.js to body.js

---
+++ classtable/body.js
@@ -0,0 +1,3 @@
+var w = new Date();
+var day = w.getDay() + 1;
+document.write("<table border='3'><tr><th>时段</th><th>时间</th><th");
--- classtable/main.js
+++
@@ -1,3 +0,0 @@
-var w = new Date();
-var day = w.getDay() + 1;
-document.write("<table border='3'><tr>");
