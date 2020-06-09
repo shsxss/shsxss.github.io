e6ccc4ecab153706d47c2764c6767eec16b251ae
MapMaths <62785981+MapMaths@users.noreply.github.com>
2020-06-10 06:39:51 +0800

    Update body.js

--- classtable/body.js
+++ classtable/body.js
@@ -1,6 +1,6 @@
 var w = new Date();
 var day = w.getDay();
-document.write("<table border='3'><tr><th class='bblue'>时段</th><th class='bblue'>时间</th><th class='bblue'>节数</th>");
+document.write("<div><table border='3'><caption><h1>课程表</h1></caption><tr><th class='bblue'>时段</th><th class='bblue'>时间</th><th class='bblue'>节数</th>");
 if (day == 1){
   document.write("<th class='red'>星期一</th>");
 }
@@ -34,3 +34,5 @@ else{
 document.write("<tr><td>上午</td><td>7：10-7：50</td><td>早管</td><td>数学</td><td>语文</td><td>英语</td><td>数学</td><td>语文</td></tr>");
 document.write("<tr><td>上午</td><td>8：00-8：40</td><td>第一节</td><td>语文</td><td>英语</td><td>语文</td><td>英语</td><td>数学</td></tr>");
 document.write("<tr><td>上午</td><td>9：10-9：50</td><td>第二节</td><td>劳技</td><td>数学</td><td>科学</td><td>语文</td><td>体育</td></tr>");
+document.write("<tr><td>上午</td><td>10：10-10：50</td><td>第三节</td><td>数学</td><td>科学</td><td>劳技</td><td>音乐</td><td>英语</td></tr>");
+document.write("<tr><td>上午</td><td>10：10-10：50</td><td>第四节</td><td>英语</td><td>信息科技</td><td>数学</td><td>体育</td><td>语文</td></tr>");
